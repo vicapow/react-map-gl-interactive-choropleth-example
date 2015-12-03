0a5cb0571953e93aba7c87e9c10d210a88df2f46
Victor Powell <vp@uber.com>
2015-12-03 12:17:12 -0800

    Create an example of an interactive choropleth.

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 
-# A simple playground to experiment with react-map-gl
+# A example of creating a choropelth with react-map-gl
+
+See the [live demo](http://vicapow.github.io/react-map-gl-interactive-choropleth-example).
+
+Note, this technique isn't specific to react-map-gl. You can do the same
+technique by using [MapboxGL-js](https://github.com/mapbox/mapbox-gl-js).
 
 ## Installation
 
     npm install
-
 
 ## To run
 
@@ -18,3 +22,7 @@ This will start a local server running on [localhost:9966](http://localhost:9966
  under [CC BY 3.0](http://creativecommons.org/licenses/by/3.0). Data by
 [OpenStreetMap](http://openstreetmap.org), under
 [CC BY SA](http://creativecommons.org/licenses/by-sa/3.0).
+
+Example GeoJSON data from: [SF OpenData](http://data.sfgov.org).
+
+Colors from [Flat UI](https://flatuicolors.com/).
--- index.js
+++ index.js
@@ -10,7 +10,39 @@ var document = require('global/document');
 var window = require('global/window');
 var rasterTileStyle = require('raster-tile-style');
 var tileSource = '//tile.stamen.com/toner/{z}/{x}/{y}.png';
-var mapStyle = Immutable.fromJS(rasterTileStyle([tileSource]));
+var mapStyle = rasterTileStyle([tileSource]);
+
+var geoJSON = require('./features-example-sf.json');
+
+mapStyle.sources['our-choropleth-source'] = {type: 'geojson', data: geoJSON};
+
+geoJSON.features.forEach(function each(feature) {
+  feature.properties.foobar = Math.random();
+});
+
+mapStyle.layers.push({
+  id: 'geojson-sf-boobar-low',
+  type: 'fill',
+  source: 'our-choropleth-source',
+  interactive: true,
+  filter: ['>=', 'foobar', 0.5],
+  paint: {
+    'fill-color': '#e74c3c',
+    'fill-opacity': 0.4
+  }
+});
+
+mapStyle.layers.push({
+  id: 'geojson-sf-boobar-hi',
+  type: 'fill',
+  source: 'our-choropleth-source',
+  interactive: true,
+  filter: ['<', 'foobar', 0.5],
+  paint: {
+    'fill-color': '#2ecc71',
+    'fill-opacity': 0.4
+  }
+});
 
 var App = React.createClass({
   getInitialState: function getInitialState() {
@@ -19,10 +51,11 @@ var App = React.createClass({
         latitude: 37.78,
         longitude: -122.45,
         zoom: 11,
-        mapStyle: mapStyle,
+        mapStyle: Immutable.fromJS(mapStyle),
         width: window.innerWidth,
         height: window.innerHeight
-      }
+      },
+      hoveredFeatures: []
     };
   },
 
@@ -41,10 +74,46 @@ var App = React.createClass({
     this.setState({map: assign({}, this.state.map, opt)});
   },
 
+  _onHoverFeatures: function _onHoverFeatures(features) {
+    this.setState({hoveredFeatures: features});
+  },
+
   render: function render() {
-    return r(MapGL, assign({
-      onChangeViewport: this._onChangeViewport
-    }, this.state.map));
+    return r.div({}, [
+      r.code({
+        style: {
+          position: 'absolute',
+          left: 10,
+          top: 10,
+          color: 'white',
+          zIndex: 1
+        }
+      }, r.pre(JSON.stringify(this.state.hoveredFeatures, null, 2))),
+      r(MapGL, assign({
+        onChangeViewport: this._onChangeViewport,
+        onHoverFeatures: this._onHoverFeatures
+      }, this.state.map)),
+      r.div({
+        style: {
+          position: 'absolute',
+          right: 10,
+          bottom: 10,
+          fontFamily: 'Helvetica',
+          background: 'white',
+          padding: 4
+        }
+      }, [
+        'Map tiles by ',
+        r.a({href: 'http://stamen.com'}, 'Stamen'),
+        ' under ',
+        r.a({href: 'http://creativecommons.org/licenses/by/3.0'}, 'CC BY 3.0'),
+        '. Data by ',
+        r.a({href: 'http://openstreetmap.org'}, 'OpenStreetMap'),
+        ', under ',
+        r.a({href: 'http://www.openstreetmap.org/copyright'}, 'ODbL'),
+        '.'
+      ])
+    ]);
   }
 });
 
